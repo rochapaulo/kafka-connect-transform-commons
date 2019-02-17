9136c2cccfa31baac07c403131ac40fd6bf5d7d9
paulo.almeida <paulo.almeida@gamesys.co.uk>
2019-02-17 17:03:07 +0000

    license

--- src/main/java/almeida/paulorocha/kafka/connect/transform/common/BaseExtractorVisitor.java
+++ src/main/java/almeida/paulorocha/kafka/connect/transform/common/BaseExtractorVisitor.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2019 Paulo Almeida (almeida.paulorocha@outlook.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package almeida.paulorocha.kafka.connect.transform.common;
 
 import java.util.Arrays;
@@ -11,7 +26,7 @@ abstract class BaseExtractorVisitor<T> {
   private final String delimiter;
   private final VisitorFunction<T> visitorFunction;
 
-  final <R> R extract(T startNode, String nestedNodePath) {
+  final <R> R extract(T startNode, String nestedNodePath) throws ClassCastException {
     String[] arr = nestedNodePath.split(delimiter);
     log.info("Walking through: {}", Arrays.toString(arr));
     return getValue(visit(startNode, arr, 0, arr.length), arr[arr.length - 1]);
@@ -25,7 +40,7 @@ abstract class BaseExtractorVisitor<T> {
     }
   }
 
-  abstract <R> R getValue(T startNode, String field);
+  abstract <R> R getValue(T startNode, String field) throws ClassCastException;
 
   @FunctionalInterface
   interface VisitorFunction<T> {
--- src/main/java/almeida/paulorocha/kafka/connect/transform/common/StructValueBaseExtractor.java
+++ src/main/java/almeida/paulorocha/kafka/connect/transform/common/StructValueBaseExtractor.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2019 Paulo Almeida (almeida.paulorocha@outlook.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package almeida.paulorocha.kafka.connect.transform.common;
 
 import org.apache.kafka.connect.data.Struct;
@@ -10,7 +25,7 @@ class StructValueBaseExtractor extends BaseExtractorVisitor<Struct> {
   }
 
   @Override
-  <T> T getValue(Struct node, String field) {
+  <T> T getValue(Struct node, String field) throws ClassCastException {
     return (T) node.get(field);
   }
 
